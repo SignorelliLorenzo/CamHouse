2c72747e5ca90e67579b490f5bce6cd3fe6638ae
GhilardiAndrea <andrea.ghilardi.03@gmail.com>
2022-01-18 11:50:42 +0100

    Creazione ramo e Aggiunta UserModel

---
+++ Api_Pcto/Api_Pcto/Models/Servizi/ITelecamereService.cs
@@ -0,0 +1,22 @@
+﻿using Api_Pcto.Models.DTOS.Requests;
+using Api_Pcto.Models.DTOS.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_Pcto.Models
+{
+    public interface ITelecamereService
+    {
+        public Task<IEnumerable<Telecamera_Data>> GetAll();
+
+        public Task<GetTelecameraPerIdResponse> GetById(int id);
+
+        public Task<CreaTelecameraResponse> Post(CreaTelecameraRequest request);
+
+        public Task<ModificaTelecameraResponse> Put(ModificaTelecameraRequest request);
+
+        public Task<EliminaTelecameraResponse> Delete(int id);
+    }
+}
