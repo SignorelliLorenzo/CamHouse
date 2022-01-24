ea842c3f55b40cef3f34892a1c700449a47ff44e
GhilardiAndrea <andrea.ghilardi.03@gmail.com>
2022-01-24 09:23:13 +0100

    Aggiunta summary nel controller

--- Api_Pcto/Api_Pcto/Controllers/TelecamereController.cs
+++ Api_Pcto/Api_Pcto/Controllers/TelecamereController.cs
@@ -23,6 +23,10 @@ namespace Api_Pcto.Controllers
             _telecamere = telecamere;
         }
         // GET: api/<ValuesController>
+        /// <summary>
+        /// Metodo API che restituisce l'intero contenuto del Database.
+        /// </summary>
+        /// <returns>IEnumerable<Telecamera_Data></returns>
         [HttpGet]
         public async Task<IEnumerable<Telecamera_Data>> GetAll()
         {
@@ -32,6 +36,11 @@ namespace Api_Pcto.Controllers
         }
 
         // GET api/<ValuesController>/5
+        /// <summary>
+        /// Metodo API che restituisce un oggetto DTO GetTelecameraPerIdResponse dato un id.
+        /// </summary>
+        /// <param name="id">Id della telecamera interessata.</param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<GetTelecameraPerIdResponse>> GetById(int id)
         {
@@ -41,6 +50,11 @@ namespace Api_Pcto.Controllers
             return result;
         }
 
+        /// <summary>
+        /// Metodo API che crea un oggetto DTO CreaTelecameraResponse e crea una telecamera sul database dato un DTO CreaTelecameraRequest.
+        /// </summary>
+        /// <param name="request">Oggetto CreaTelecameraRequest, maggiori informazioni reperibili tramite schema su swagger</param>
+        /// <returns></returns>
         // POST api/<ValuesController>
         [HttpPost]
         public async Task<ActionResult<CreaTelecameraResponse>> Post(CreaTelecameraRequest request)
@@ -51,7 +65,13 @@ namespace Api_Pcto.Controllers
             return result;
         }
 
+
         // PUT api/<ValuesController>/5
+        /// <summary>
+        /// Metodo API che crea un oggetto DTO ModificaTelecameraResponse e modifica una telecamera sul database dato un DTO ModificaTelecameraRequest.
+        /// </summary>
+        /// <param name="request">Oggetto ModificaTelecameraRequest, maggiori informazioni reperibili tramite schema su swagger</param>
+        /// <returns></returns>
         [HttpPut]
         public async Task<ActionResult<ModificaTelecameraResponse>> Put(ModificaTelecameraRequest request)
         {
@@ -62,6 +82,11 @@ namespace Api_Pcto.Controllers
         }
 
         // DELETE api/<ValuesController>/5
+        /// <summary>
+        /// Metodo API che restituisce un oggetto DTO EliminaTelecameraResponse ed elimina dal database una telecamera dato il suo id.
+        /// </summary>
+        /// <param name="id">Id della telecamera interessata.</param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         public async Task<ActionResult<EliminaTelecameraResponse>> Delete(int id)
         {
