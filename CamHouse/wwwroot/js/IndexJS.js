d561db1d16fe4c29f276df93d9fe5b9f4ab6a9c3
Daniele Tudorachi <danieletudorachi007@gmail.com>
2022-02-14 20:28:46 +0100

    Version 0.3: Sviluppo interfaccia Index

--- CamHouse/wwwroot/js/IndexJS.js
+++ CamHouse/wwwroot/js/IndexJS.js
@@ -4,3 +4,15 @@ function RemovePageLoader(){
     document.getElementById("loader-body").style.display = "none";
     document.getElementById("BodyCustomIndex").style.backgroundColor = "#c0fce4"
 }
+
+function RegisterImgChange() {
+    document.getElementById("imgindex").src = "/Img/indexregister.png";
+}
+
+function LoginImgChange() {
+    document.getElementById("imgindex").src = "/Img/indexlogin.png";
+}
+
+function ResetImgChange() {
+    document.getElementById("imgindex").src = "/Img/index.png";
+}
